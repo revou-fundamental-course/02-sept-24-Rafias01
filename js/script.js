1376518b1f5937b232ed23541d9891867793052a
Rafias01 <rafiasubekti@gmail.com>
2024-09-11 21:29:11 +0700

    next configuration

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//ini Javascript
+// //ini Javascript
+
+// let inisiasi = 0;
+
+// if(inisiasi == 1 ){
+//     console.log('benar');
+// } else{
+//     console.log('salah')
+// }
+
+function luasSegitiga(){
+    let alas = parseInt(document.getElementById("panjang-alas1").value);
+    let tinggi = parseInt(document.getElementById("tinggi1").value);
+
+    
+}
+
